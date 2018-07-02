ee43eb1beb30feaf83d28a34446e9552ddf29ccb
Administrator <842404548@qq.com>
2018-07-02 16:58:48 +0800

    application.yml

--- eurekaserver/src/main/java/com/cloud/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/cloud/eurekaserver/EurekaserverApplication.java
@@ -2,8 +2,11 @@ package com.cloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//启动一个服务注册中心
+@EnableEurekaServer
 public class EurekaserverApplication {
 
     public static void main(String[] args) {
