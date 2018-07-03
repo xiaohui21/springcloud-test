6bed090e20fcef74be9874a5d7cc3a2887800199
Administrator <842404548@qq.com>
2018-07-03 14:37:09 +0800

    ribbon

--- eurekaclient/src/main/java/com/cloud/eurekaclient/EurekaclientApplication.java
+++ eurekaclient/src/main/java/com/cloud/eurekaclient/EurekaclientApplication.java
@@ -1,15 +1,30 @@
 package com.cloud.eurekaclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//
+//通过注解@EnableEurekaClient 表明自己是一个eurekaclient.
 @EnableEurekaClient
+@RestController
 public class EurekaclientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaclientApplication.class, args);
     }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi " + name + ",i am from port:" + port;
+    }
+
+
 }
