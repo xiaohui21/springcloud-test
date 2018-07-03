647c79bba054b4826ca99d01bee85a6ff2435d8a
Administrator <842404548@qq.com>
2018-07-03 14:38:44 +0800

    ribbon

---
+++ ribbon/src/main/java/com/server/ribbon/RibbonApplication.java
@@ -0,0 +1,27 @@
+package com.server.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//通过@EnableDiscoveryClient向服务中心注册
+@EnableDiscoveryClient
+public class RibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class, args);
+    }
+
+    /**
+     * 向程序的ioc注入一个bean: restTemplate;并通过@LoadBalanced注解表明这个restRemplate开启负载均衡的功能。
+     */
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ ribbon/src/main/java/com/server/ribbon/contoller/HelloController.java
@@ -0,0 +1,24 @@
+package com.server.ribbon.contoller;
+
+import com.server.ribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: RoronoaZoro丶WangRui
+ * Time: 2018/7/3/003
+ * Describe:
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService(name);
+    }
+}
