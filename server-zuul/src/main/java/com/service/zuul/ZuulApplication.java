2d4a62812ad0775dfc8b24e356900e16f83cb781
Administrator <842404548@qq.com>
2018-07-03 17:12:32 +0800

    路由过滤功能

--- server-zuul/src/main/java/com/service/zuul/ZuulApplication.java
+++ server-zuul/src/main/java/com/service/zuul/ZuulApplication.java
@@ -2,8 +2,12 @@ package com.service.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableZuulServer
 public class ZuulApplication {
 
     public static void main(String[] args) {
