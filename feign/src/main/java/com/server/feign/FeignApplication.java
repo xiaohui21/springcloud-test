6ef44c702e07de6485bd4de68636643010ed3bdf
Administrator <842404548@qq.com>
2018-07-19 09:14:39 +0800

    turbine

--- feign/src/main/java/com/server/feign/FeignApplication.java
+++ feign/src/main/java/com/server/feign/FeignApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
@@ -15,6 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableTurbine
 public class FeignApplication {
 
     public static void main(String[] args) {
--- server-config/src/main/java/com/server/config/ConfigApplication.java
+++ server-config/src/main/java/com/server/config/ConfigApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigApplication {
 
     public static void main(String[] args) {
